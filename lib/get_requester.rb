d915ce805ceb81acf8ed2ec704e33522e4ab3d46
Cinna Goldman <somethingfellipickeditup@gmail.com>
2020-09-02 19:19:26 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,12 @@
-# Write your code here
+class GetRequester
+
+  def initialize(url)
+
+  end
+
+  def get_response_body
+    # sends a GET request to the URL passed in on initialization. This method should return the body of the response.
+
+  end
+
+end
