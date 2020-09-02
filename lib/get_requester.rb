e66a73fce5dd155fca466310daa94ca3a60b6f16
Cinna Goldman <somethingfellipickeditup@gmail.com>
2020-09-02 19:19:53 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -6,7 +6,10 @@ class GetRequester
 
   def get_response_body
     # sends a GET request to the URL passed in on initialization. This method should return the body of the response.
+  end
 
+  def parse_json
+    # should use get_response_body to send a request, then return a Ruby Array or Hash made up of data converted from the response of that request.
   end
 
 end
